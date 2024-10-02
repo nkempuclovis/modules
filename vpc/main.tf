db3b19bf70948f0e899ee5e4558d00b5ecffd9e3
Clovis Foji <nkempu20@yahoo.com>
2024-10-02 07:53:04 -0400

    Updated VPC

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
+  instance_tenancy = default
+  enable_dns_support = true
   tags = {
     Name = "${var.env}-vpc"
     Environment = var.env
